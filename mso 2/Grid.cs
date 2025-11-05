0b4dda2f597b46f5cd38f539aca0ede939c80221
stormbosman <bosmanstorm@gmail.com>
2025-11-05 22:23:41 +0100

    testing commit with part of Grid

--- mso 2/Grid.cs
+++ mso 2/Grid.cs
@@ -12,7 +12,7 @@ namespace mso_2
     {
         private int _width;
         private int _height;
-        private bool[,] _occupied;
+        public bool[,] _occupied { get; set; }
 
         public Grid(int width, int height)
         {
@@ -20,10 +20,38 @@ namespace mso_2
             _height = height;
             _occupied = new bool[width, height];
         }
-
-        bool TryMove(MoveEntity entity, Vector2 direction, int steps) 
+        public void SwitchOccupied(int x, int y) 
         {
+            //switch the boolean value in the occupied grid at the location xy
+            this._occupied[x, y] = !this._occupied[x, y];
+        }
+
+        public void ImportOccupied() 
+        { 
+            //dunno what we wanna do for reading in files yet 
+            //todo
+        }
+
+        public bool TryMove(MoveEntity entity, int steps) 
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                if (CheckAhead(entity)) 
+                { 
+                    
+                }
+            }
             return true;
+        }
+        //find the position ahead of the moveentity, and see if it is occupied
+        public bool CheckAhead(MoveEntity entity) 
+        {
+            Vector2 ahead = entity.position + entity.direction;
+            return (_occupied[(int)ahead.X, (int)ahead.Y]&&CheckBounds(ahead));     
+        }
+        public bool CheckBounds(Vector2 position)
+        {
+            return (position.X <= _width && position.Y <= _height);
         }
     }
 }
